37755f0b47931779b3616a5f7f11dc9b012d6cb2
Shtankevich Dima <schtanckevitch.dima@yandex.ru>
2025-12-10 14:02:04 +0300

    add new folder and mvp form for feeadback

---
+++ src/scripts/api.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const URL = 'https://sheetdb.io/api/v1/5c5trat7uaako';
+
+  const form = document.getElementById('feedback-form');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const comments = await fetch(URL)
+      .then((res) => res.json());
+
+    const lastId = await Number(comments[comments.length - 1].id);
+
+    // Способ создать один комментарий
+    fetch('https://sheetdb.io/api/v1/5c5trat7uaako', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data: {
+          'id': lastId + 1,
+          'name': formData.get('name'),
+          'comment': formData.get('comment'),
+        }
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+}); 
